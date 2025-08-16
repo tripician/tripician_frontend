be418f31570369545390b85ceb08c39b45250cb9
srideep <srideepkar@gmail.com>
2025-08-17 00:02:10 +0530

    refactor

--- src/components/pages/ProfilePage/ProfileBadges.tsx
+++ src/components/pages/ProfilePage/ProfileBadges.tsx
@@ -54,50 +54,6 @@ export default function ProfileBadges() {
     {
       country : "Vietnam",
       flagUrl : "https://res.cloudinary.com/ddt3rcyhv/image/upload/v1755240289/Vietnam_cagk5x.png"
-    },
-    {
-      country : "Vietnam",
-      flagUrl : "https://res.cloudinary.com/ddt3rcyhv/image/upload/v1755240289/Vietnam_cagk5x.png"
-    },
-    {
-      country : "Vietnam",
-      flagUrl : "https://res.cloudinary.com/ddt3rcyhv/image/upload/v1755240289/Vietnam_cagk5x.png"
-    },
-    {
-      country : "Vietnam",
-      flagUrl : "https://res.cloudinary.com/ddt3rcyhv/image/upload/v1755240289/Vietnam_cagk5x.png"
-    },
-    {
-      country : "Vietnam",
-      flagUrl : "https://res.cloudinary.com/ddt3rcyhv/image/upload/v1755240289/Vietnam_cagk5x.png"
-    },
-    {
-      country : "Vietnam",
-      flagUrl : "https://res.cloudinary.com/ddt3rcyhv/image/upload/v1755240289/Vietnam_cagk5x.png"
-    },
-    {
-      country : "Vietnam",
-      flagUrl : "https://res.cloudinary.com/ddt3rcyhv/image/upload/v1755240289/Vietnam_cagk5x.png"
-    },
-    {
-      country : "Vietnam",
-      flagUrl : "https://res.cloudinary.com/ddt3rcyhv/image/upload/v1755240289/Vietnam_cagk5x.png"
-    },
-    {
-      country : "Vietnam",
-      flagUrl : "https://res.cloudinary.com/ddt3rcyhv/image/upload/v1755240289/Vietnam_cagk5x.png"
-    },
-    {
-      country : "Vietnam",
-      flagUrl : "https://res.cloudinary.com/ddt3rcyhv/image/upload/v1755240289/Vietnam_cagk5x.png"
-    },
-    {
-      country : "Vietnam",
-      flagUrl : "https://res.cloudinary.com/ddt3rcyhv/image/upload/v1755240289/Vietnam_cagk5x.png"
-    },
-    {
-      country : "Vietnam",
-      flagUrl : "https://res.cloudinary.com/ddt3rcyhv/image/upload/v1755240289/Vietnam_cagk5x.png"
     }
 ];
 
